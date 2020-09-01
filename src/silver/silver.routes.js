2c0222cc42ad9e56936a36d1acf079a65f022b7d
Tarun Jajoria <tarunjajoria21@gmail.com>
2020-09-01 22:43:26 +0530

    made few chages and removed few routers

--- src/silver/silver.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const { registersilver, allsilver } = require('./silver.controller')
-
-router.get('/all-silver', allsilver)
-router.post('/register', registersilver)
-
-module.exports = router
